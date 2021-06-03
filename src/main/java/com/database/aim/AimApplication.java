dcb21c9e0126b6645bd3669d2e895deb9420aba8
lynn <sun_0901@foxmail.com>
2021-06-03 10:28:29 +0800

    sjf0603init

---
+++ src/main/java/com/database/aim/AimApplication.java
@@ -0,0 +1,13 @@
+package com.database.aim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AimApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AimApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/database/aim/AimApplicationTests.java
@@ -0,0 +1,13 @@
+package com.database.aim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AimApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
